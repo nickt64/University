d5a20c30e1bc469b292c634729a2217110cfc573
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2023-01-29 15:50:13 -0300

    Clase Curso Segun requerimientos

---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Curso
+    {
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required,]
+        public string LongDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetAudiences { get; set; } = string.Empty;
+
+        [Required]
+        public string Objectives { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required]
+        public Levels Level { get; set; } = Levels.None;
+
+        
+    }
+    public enum Levels
+    {
+        None,
+        Basico,
+        Intermedio,
+        Avanzado
+    };
+}
